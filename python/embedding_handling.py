1fb2923a4d59f09ea3ee9805315a3a4fde37495f
Malcolm Keyes <malcolm.a.keyes@gmail.com>
2023-11-07 21:37:11 -0500

    feat: initialize file store & vector store

---
+++ python/embedding_handling.py
@@ -0,0 +1,79 @@
+import uuid
+
+import chromadb
+from dotenv import load_dotenv
+from langchain.document_loaders import UnstructuredMarkdownLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+import file_handling
+
+
+CHUNK_SIZE = 512
+CHUNK_OVERLAP = 24
+
+
+load_dotenv()
+
+
+def get_embeddings_model() -> OpenAIEmbeddings:
+    """Returns a LangChain OpenAIEmbeddings model."""
+
+    return OpenAIEmbeddings(model="text-embedding-ada-002")
+
+
+def get_vector_store_collection() -> chromadb.Collection:
+    """Returns a client connection to the collection in the vector store. Creates both
+    the vector store and the collection if they do not exist.
+    """
+
+    client = chromadb.PersistentClient(path="./.chromadb/")
+    collection = client.get_or_create_collection(
+        name="athena",
+        metadata={"hnsw:space": "cosine"},
+    )
+
+    return collection
+
+
+def save_note_embeddings_to_vector_store() -> None:
+    """Create the embeddings for the notes in the vault and save them to the vector
+    store. Creates the vector store if it does not exist.
+
+    Also creates the note file store if it does not exist.
+    """
+
+    if not file_handling.file_store_exists():
+        file_handling.create_new_note_store()
+
+    file_store = file_handling.load_file_store()
+
+    if not isinstance(file_store, dict):
+        print("Loading file store failed. Embedding creation skipped.")
+
+    collection = get_vector_store_collection()
+    embedding_model = get_embeddings_model()
+
+    for file_name in file_store:
+        loader = UnstructuredMarkdownLoader(f"../../../../{file_name}")
+        chunks = loader.load_and_split(
+            CharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=CHUNK_SIZE,
+                chunk_overlap=CHUNK_OVERLAP,
+            )
+        )
+
+        file_store[file_name]["chunk_size"] = CHUNK_SIZE
+        file_store[file_name]["chunk_overlap"] = CHUNK_OVERLAP
+
+        for chunk in chunks:
+            chunk_uuid = str(uuid.uuid4())
+
+            file_store[file_name]["chunks"].append(chunk_uuid)
+
+            chunk_embedding = embedding_model.embed_documents([chunk.page_content])
+
+            collection.add([chunk_uuid], chunk_embedding, [{"document_uuid": file_store[file_name]["uuid"]}], [file_name])
+
+    file_handling.update_file_store(file_store)
---
+++ python/file_handling.py
@@ -0,0 +1,61 @@
+import glob
+import json
+import os
+import uuid
+
+
+def collect_files_from_vault() -> list[str]:
+    """Returns the file names of the markdown files in the vault directory."""
+
+    return glob.glob("../../../../*.md")
+
+
+def create_new_note_store(overwrite_existing_store: bool = False) -> None:
+    """Creates a new json file to store information about the notes. Does nothing if
+    there is already an existing store and overwrite_existing_store is False.
+    """
+
+    if os.path.exists("file_store.json") and not overwrite_existing_store:
+        print("File store already exists. Skipping file store creation.")
+        return
+    
+    file_store = {}
+    
+    notes = collect_files_from_vault()
+
+    for note in notes:
+        file_store[os.path.basename(note)] = {
+            "last_modified" : os.path.getmtime(note),
+            "uuid" : str(uuid.uuid4()), # uuid object is not serializable
+            "chunks" : []
+        }
+
+    with open("file_store.json", "w") as json_file:
+        json.dump(file_store, json_file)
+
+
+def file_store_exists() -> bool:
+    """Checks the existence of the file store."""
+
+    return os.path.exists("file_store.json")
+
+
+def load_file_store() -> dict | None:
+    """Returns a loaded dictionary of the file store. If the file store does not exist,
+    then returns None.
+    """
+
+    if not file_store_exists():
+        return None
+    
+    with open("file_store.json", "r") as json_file:
+        loaded = json.load(json_file)
+
+    return loaded
+
+
+def update_file_store(file_store: dict) -> None:
+    """Overwrite the file store with a dictionary."""
+
+    with open("file_store.json", "w") as json_file:
+        json.dump(file_store, json_file)
