2819e15ea64d4d4aa970b3e0d49c805b220d2e92
Malcolm Keyes <malcolm.a.keyes@gmail.com>
2023-12-11 23:21:08 -0500

    chore: remove dead code from main.ts

--- main.ts
+++ main.ts
@@ -11,41 +11,24 @@ import {
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface AthenaSettings {
 	openaiApiKey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: AthenaSettings = {
 	openaiApiKey: "",
 };
 
 export default class AthenaPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: AthenaSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Athena",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice from Athena!");
-			}
-		);
-
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Athena Online");
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: "open-athena-modal-simple",
-			name: "Open Athena modal (simple)",
+			id: "open-athena-chat-modal",
+			name: "Open Athena chat interface",
 			callback: () => {
 				new SampleModal(this.app).open();
 			},
@@ -84,15 +67,6 @@ export default class AthenaPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {}
