94efc75ae44391376b655b38804eabfb7227669a
Malcolm Keyes <malcolm.a.keyes@gmail.com>
2023-12-11 23:05:46 -0500

    style: prettier main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
@@ -7,8 +16,8 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	openaiApiKey: ''
-}
+	openaiApiKey: "",
+};
 
 export default class AthenaPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -17,44 +26,49 @@ export default class AthenaPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Athena', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice from Athena!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Athena",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice("This is a notice from Athena!");
+			}
+		);
 
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Athena Online');
+		statusBarItemEl.setText("Athena Online");
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-athena-modal-simple',
-			name: 'Open Athena modal (simple)',
+			id: "open-athena-modal-simple",
+			name: "Open Athena modal (simple)",
 			callback: () => {
 				new SampleModal(this.app).open();
-			}
+			},
 		});
 
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'athena-editor-command',
-			name: 'Athena simple editor command',
+			id: "athena-editor-command",
+			name: "Athena simple editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Athena Editor Command');
-			}
+				editor.replaceSelection("Athena Editor Command");
+			},
 		});
 
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-athena-modal-complex',
-			name: 'Open Athena modal (complex)',
+			id: "open-athena-modal-complex",
+			name: "Open Athena modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -65,7 +79,7 @@ export default class AthenaPlugin extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -81,24 +95,19 @@ export default class AthenaPlugin extends Plugin {
 		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-		await this.saveApiKeyToFile(this.settings.openaiApiKey);
 	}
-
-	async saveApiKeyToFile(apiKey: string) {
-        const fileContent = `OPENAI_API_KEY=${apiKey}\n`;
-        const filePath = this.app.vault.adapter.basePath + '/python/.env';
-        await this.app.vault.adapter.write(filePath, fileContent);
-    }
 }
 
 class SampleModal extends Modal {
@@ -107,12 +116,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -126,19 +135,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('OpenAI API Key')
-			.setDesc('This is necessary for functioning of the plugin')
-			.addText(text => text
-				.setPlaceholder('OpenAI API Key')
-				.setValue(this.plugin.settings.openaiApiKey)
-				.onChange(async (value) => {
-					this.plugin.settings.openaiApiKey = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("OpenAI API Key")
+			.setDesc("This is necessary for functioning of the plugin")
+			.addText((text) =>
+				text
+					.setPlaceholder("OpenAI API Key")
+					.setValue(this.plugin.settings.openaiApiKey)
+					.onChange(async (value) => {
+						this.plugin.settings.openaiApiKey = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
